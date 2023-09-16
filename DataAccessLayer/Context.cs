02770de88fdf845474e8dd75a8a352469229eda7
murad khasmammadov <murad.07mx@gmail.com>
2023-09-16 16:51:23 +0300

    files uploaded

---
+++ DataAccessLayer/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server = DESKTOP-28FHEV5; database = CoreBlogApiDb; integrated security = true; Encrypt=False;");
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
